7686e93b6d44d3ded60091debc27c5a05b2b929f
rollytiberius <rollytiberius@gmail.com>
2024-10-19 21:30:31 +0100

    Another update of js

--- FccForum/script.js
+++ FccForum/script.js
@@ -1,2 +1,13 @@
 const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json"
+const forumTopicUrl = "https://forum.freecodecamp.org/t/"
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/"
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp"
+const postsContainer = document.getElementById("posts-container")
 
+const fetchData = async () => {
+    try {
+        const res = await fetch(forumLatest)
+    }catch (err) {
+        console.log(err)
+    }
+}
